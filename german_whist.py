193fd26d46c73b676cbd3d0c204d611e16740cb1
Max <Max.beales@gmail.com>
2023-07-20 17:07:28 +0200

    add empty player hands as states in init

--- german_whist.py
+++ german_whist.py
@@ -65,7 +65,9 @@ class Play:
         self.ranking_dict = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, 
                 "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, 
                 "Q": 12, "K": 13, "A": 14} # this never actually changes
-        
+        self.player1_hand = []
+        self.player2_hand = []
+
     def copy(self):
         return deepcopy(self)
 
